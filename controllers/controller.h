672acb80fa16175d87a0795922e4f0c3a8991291
Daniel <daniel032@binus.ac.id>
2022-05-12 17:22:57 +0700

    Push project files

---
+++ controllers/controller.h
@@ -0,0 +1,304 @@
+#include "../models/model.h"
+#include <math.h>
+
+// Untuk menampilkan OS
+const char* detectOS() {
+  #ifdef _WIN32
+  return "Windows 32-bit";
+  #elif _WIN64
+  return "Windows 64-bit";
+  #elif __APPLE__ || __MACH__
+  return "Mac OSX";
+  #elif __linux__
+  return "Linux";
+  #elif __FreeBSD__
+  return "FreeBSD";
+  #elif __unix || __unix__
+  return "Unix";
+  #else
+  return "Other";
+  #endif
+}
+
+// Untuk membuat node dish baru
+Dish *createDish(char *name, int price, int qty) {
+  Dish *temp = (Dish *)malloc(sizeof(Dish));
+  strcpy(temp->name, name);
+  temp->price = price;
+  temp->qty = qty;
+  temp->next = temp->prev = NULL;
+  return temp;
+}
+
+// Untuk membuat node customer baru
+Customer *createCustomer(char *name) {
+  Customer *temp = (Customer *)malloc(sizeof(Customer));
+  strcpy(temp->name, name);
+  temp->prevCust = temp->nextCust = NULL;
+  return temp;
+}
+
+// Untuk membuat node order baru
+Order *createOrder(char *name, int qty, int price) {
+  Order *temp = (Order *)malloc(sizeof(Order));
+  strcpy(temp->name, name);
+  temp->qty = qty;
+  temp->price = price;
+  temp->nextOrder = temp->prevOrder = NULL;
+  return temp;
+}
+
+// Untuk push node dish ke head
+void pushHead(char *name, int price, int qty) {
+  Dish *temp = createDish(name, price, qty);
+  if(!head) {
+    head = tail = temp;
+  }
+  else {
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+  }
+}
+
+// Untuk push node dish ke tail
+void pushTail(char *name, int price, int qty) {
+  Dish *temp = createDish(name, price, qty);
+  if(!head) {
+    head = tail = temp;
+  }
+  else {
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+  }
+}
+
+// Priority queue atau push sekaligus ngesort sesuai quantity dari node dish
+void pushMid(char *name, int price, int qty) {
+  Dish *temp = createDish(name, price, qty);
+  if(!head) {
+    head = tail = temp;
+  }
+  else if(qty <= head->qty) {
+    pushHead(name, price, qty);
+  }
+  else if(qty >= tail->qty) {
+    pushTail(name, price, qty);
+  }
+  else {
+    curr = head;
+    while(qty > curr->qty) {
+      curr = curr->next;
+    }
+    temp->prev = curr->prev;
+    temp->next = curr;
+    curr->prev->next = temp;
+    curr->prev = temp;
+  }
+}
+
+// Untuk membebaskan node head
+void popHead() {
+  if(!head) {
+    return;
+  }
+  else if(head == tail) {
+    head = tail = NULL;
+    free(head);
+  }
+  else {
+    Dish *temp = head;
+    head = head->next;
+    head->prev = NULL;
+    free(temp);
+  }
+}
+
+// Untuk membebaskan node tail
+void popTail() {
+  if(!head) {
+    return;
+  }
+  else if(head == tail) {
+    head = tail = NULL;
+    free(head);
+  }
+  else {
+    Dish *temp = tail;
+    tail = tail->prev;
+    tail->next = NULL;
+    free(temp);
+  }
+}
+
+// Untuk membebaskan node berdasarkan quantitynya
+void popMid(int qty) {
+  if(!head) {
+    return;
+  }
+  else if(qty == head->qty) {
+    popHead();
+  }
+  else if(qty == tail->qty) {
+    popTail();
+  }
+  else {
+    curr = head;
+    while(curr->next && qty != curr->qty) {
+      curr = curr->next;
+    }
+    curr->prev->next = curr->next;
+    curr->next->prev = curr->prev;
+    curr = NULL;
+    free(curr);
+  }
+}
+
+// Hash function
+unsigned long DJB2(char *str) {
+  unsigned long hash = 5381;
+  int c;
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) + c;
+  return hash % 26;
+}
+
+// Insert customer ke index tertentu pada table sesuai hasil hash
+void insert(char *name) {
+  int index = DJB2(name);
+  Customer *temp = createCustomer(name);
+  if(!headCust[index]) {
+    headCust[index] = tailCust[index] = temp;
+  }
+  else {
+    tailCust[index]->nextCust = temp;
+    temp->prevCust = tailCust[index];
+    tailCust[index] = temp;
+  }
+}
+
+// Untuk search customer apakah exist atau tidak
+bool search(char *name) {
+  int index = DJB2(name);
+  if(headCust[index]) {    
+    currCust = headCust[index];
+    while(currCust && name != currCust->name) {
+      if(strcmp(currCust->name, name) == 0) {
+        return true;
+      }
+      currCust = currCust->nextCust;
+    }
+    if(!currCust->nextCust) {
+      return false;
+    }
+  }
+  return false;
+}
+
+// Untuk mendapatkan posisi customer pada index tertentu dan pada node tertentu
+Customer *getNode(char *name) {
+  int index = DJB2(name);
+  if(headCust[index]) {
+    currCust = headCust[index];
+    while(currCust) {
+      if(!strcmp(currCust->name, name) == 0) {
+        return currCust;
+      }
+      currCust = currCust->nextCust;
+    }
+  }
+  return NULL;
+}
+
+// Masukin order ke list order yang ada pada list masing-masing customer
+void insertOrder(Customer *temp, char *name, int qty, int price) {
+    Order *newOrder = createOrder(name, qty, price);
+    if(!temp->headOrder) {
+      temp->headOrder = temp->tailOrder = newOrder;
+    }
+    else {
+      currOrder = temp->headOrder;
+      while(currOrder) {
+        currOrder = currOrder->nextOrder;
+      }
+      currOrder->nextOrder = newOrder;
+      newOrder->prevOrder = currOrder;
+      temp->tailOrder = newOrder;
+    }
+}
+
+// Untuk menghapus seluruh order setelah dicheckout sama customer
+void popOrder(int index) {
+  if(!headCust[index]->headOrder) {
+    return;
+  } else if(headCust[index]->headOrder == headCust[index]->tailOrder) {
+    headCust[index]->headOrder = headCust[index]->tailOrder = NULL;
+    free(headCust[index]->headOrder);
+  } else {
+    currOrder = headCust[index]->headOrder;
+    while(currOrder) {
+      currOrder = currOrder->nextOrder;
+    }
+    Order *temp = currOrder;
+    headCust[index]->tailOrder = currOrder->prevOrder;
+    currOrder->prevOrder->nextOrder = NULL;
+    free(temp);
+  }
+}
+
+// Untuk menampilkan semua nama customer pada table
+void view() {
+  for(int i = 0; i < 26; i++) {
+    if(!headCust[i]) {
+      continue;
+    } else if(headCust[i]) {
+      printf("%d. ", i);
+      currCust = headCust[i];
+      while(currCust) {
+        if(currCust)
+        printf("%s -> ", currCust->name);
+          currCust = currCust->nextCust;
+      }
+    }
+    puts("NULL");
+  }
+}
+
+bool displayDish() {
+  if(!head) {
+    puts("No menu added yet."); getchar();
+    return false;
+  }
+  puts("Bude's Menu");
+  puts("=============================================");
+  printf("%-5s %-19s %-9s %-10s\n", "No.", "Name", "Quantity", "Price");
+  int i = 1;
+  curr = head;
+  while(curr) {
+    printf("%-5d %-19s %03d %-5s Rp%-8d\n", i, curr->name, curr->qty, " ", curr->price);
+    i++;
+    curr = curr->next;
+  }
+  puts("=============================================");
+  return true;
+}
+
+// Display semua yang perlu dicheckout
+void displayPayment(int index) {
+  int i = 1, totalPrice = 0;
+  currOrder = headCust[index]->headOrder;
+  printf("%s\n", headCust[index]->name);
+  while(currOrder) {
+    printf("[%d] %s x%d\n", i, currOrder->name, currOrder->qty);
+    totalPrice += currOrder->price;
+    currOrder = currOrder->nextOrder;
+  }
+  printf("Total: Rp%d\n", totalPrice);
+}
+
+
+// Clear screen
+void cls() {
+  system("cls||clear");
+}
---
+++ view/main.cpp
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "../controllers/controller.h"
+#include "../controllers/validation.h"
+
+int totalDish = 0, dishWidth = 0;
+
+// Menu Add Dish
+void addDish() {
+  // Prompt user untuk input nama dish
+  char name[255];
+  do {
+    printf("Insert the name of the dish [Lowercase letters]: ");
+    scanf("%[^\n]", name); 
+    getchar();
+  } while(!isValidDishName(name)); // validasi input
+  if(strlen(name) > dishWidth) {
+    dishWidth = strlen(name); // untuk menampung dish dengan nama terpanjang
+  }
+  // Prompt user untuk input harga dish
+  int price;
+  do {
+    printf("Insert the price of the dish [1000..50000]: ");
+    scanf("%d", &price); 
+    getchar();
+  } while(!isValidPrice(price)); // validasi input
+  // Prompt user untuk input stok atau quantity dari dish
+  int qty;
+  do {
+    printf("Insert the quantity of the dish [1..999]: ");
+    scanf("%d", &qty); 
+    getchar();
+  } while(!isValidQty(qty)); // validasi input
+  pushMid(name, price, qty); // insert sorted/priority queue berdasarkan quantity
+  totalDish++; // untuk tracking jumlah dish yang ada
+  printf("The dish has been added!\n");
+  printf("Press enter to continue..."); 
+  getchar();
+}
+
+// Menu Remove Dish
+void removeDish() {
+  if(displayDish()) { // kalau ada menu, display
+    // Prompt user untuk input nama dish
+    char name[255];
+    do {
+      printf("Insert dish's name to be deleted: ");
+      scanf("%[^\n]", name);
+      getchar();
+    } while(!isValidDish(name)); // validasi input
+    popMid(curr->qty); // diremove sesuai quantity
+    printf("The dish has been removed!\n");
+    printf("Press enter to continue...");
+    getchar();
+  } else {
+    return;
+  }
+}
+
+// Menu Add Customer
+void addCustomer() {
+  // Prompt user untuk input nama customer
+  char name[255];
+  do {
+    printf("Insert the customer's name [Without Space]: ");
+    scanf("%[^\n]", name); 
+    getchar();
+  } while(!isValidCustName(name)); // validasi input
+  insert(name); // dihash pake djb2 dan dimasukin ke table
+  printf("Customer has been added!\n");
+  printf("Press enter to continue..."); 
+  getchar();
+}
+
+// Menu Search Customer
+void searchCustomer() {
+  // Prompt user untuk input nama customer yang ingin dicari
+  char name[255];
+  do {
+    printf("Insert the customer's name to be searched: ");
+    scanf("%[^\n]", name); 
+    getchar();
+  } while(!isValidSearchName(name));
+  (search(name)) ? printf("%s is present.\n", name) : printf("%s is not present.\n", name);
+  printf("Press enter to continue..."); 
+  getchar();
+}
+
+// Menu View Warteg
+void viewWarteg() {
+  puts("Customer's List\n");
+  view(); // display isi dari index table yang tidak null
+  printf("\nPress enter to continue..."); 
+  getchar();
+}
+
+// Menu Order
+void order() {
+  // Prompt user untuk input nama customer
+  char name[255];
+  do {
+    printf("Insert the customer's name: ");
+    scanf("%[^\n]", name); 
+    getchar();
+  } while(!search(name)); // validasi input
+  // Prompt user untuk input jumlah dish yang diinginkan
+  int amt;
+  do {
+    printf("Insert the amount of dish: ");
+    scanf("%d", &amt); 
+    getchar();
+  } while(!isValidAmt(amt, totalDish)); // validasi input
+  for(int i = 1; i <= amt; i++) { // loop sesuai banyak dish yang diinginkan
+    // Prompt user untuk pilihan dish yang diinginkan serta jumlah masing-masing dish
+    int qty, price;
+    char dishName[255];
+    do {
+      printf("[%d] Insert the dish's name and quantity: ", i);
+      scanf("%[^x]x%d", dishName, &qty); 
+      getchar();
+      // Retrieve dish pilihan dari list dan update status dish
+      curr = head;
+      while(curr) {
+        if(!strcmp(curr->name, name)) {
+          price = curr->price;
+          break;
+        }
+        curr = curr->next;
+      }
+      price *= qty;
+    } while(!isValidOrder(dishName)); // hanya jika nama invalid, maka akan mengulang
+    Customer *curr = getNode(name); // untuk mendapatkan node customer pada index dimana nama dihash
+    insertOrder(curr, dishName, qty, price); // masukkan order ke list order di dalam list customer
+    puts("Order success!");
+    printf("Press enter to continue..."); 
+    getchar();
+  }
+}
+
+// Menu Payment
+void payment() {
+  // Prompt user untuk input index yang ingin dicheckout
+  int index;
+  do {
+    printf("Insert the customer's index: ");
+    scanf("%d", &index); 
+    getchar();
+  } while(!isValidIndex(index)); // validasi input 
+  puts("");
+  displayPayment(index); // display bon dari customer pada index tertentu
+  printf("Press enter to continue..."); 
+  getchar();
+}
+
+// Menu Exit
+void splashScreen() {
+  char c;
+  puts("Please expand your terminal to full screen!");
+  printf("Press enter to continue..."); 
+  getchar();
+  cls();
+  FILE *fp;
+  fp = fopen("splash-screen.txt", "r"); // baca file berisi ascii art
+  while(fscanf(fp, "%c", &c) != EOF) { // baca per char selagi belum EOF
+    printf("%c", c); // sekalian diprint ke terminal setelah dibaca
+  }
+}
+
+void mainMenu() {
+  int input;
+  do {
+    cls();
+    printf("System: %s\n", detectOS());
+    time_t now;
+    time(&now);
+    printf("%s\n", ctime(&now));
+    puts("1. Add Dish");
+    puts("2. Remove Dish");
+    puts("3. Add customer");
+    puts("4. Search customer");
+    puts("5. View Warteg");
+    puts("6. Order");
+    puts("7. Payment");
+    puts("8. Exit warteg");
+    printf(">> ");
+    scanf("%d", &input); 
+    getchar();
+    if(input == 1) {
+      cls();
+      addDish();
+    } else if(input == 2) {
+      cls();
+      removeDish();
+    } else if(input == 3) {
+      cls();
+      addCustomer();
+    } else if(input == 4) {
+      cls();
+      searchCustomer();
+    } else if(input == 5) {
+      cls();
+      viewWarteg();
+    } else if(input == 6) {
+      cls();
+      order();
+    } else if(input == 7) {
+      cls();
+      payment();
+    } else if(input == 8) {
+      cls();
+      splashScreen();
+      break;
+    }
+  } while(input != 8);
+}
+
+int main() {
+  mainMenu();
+  return 0;
+}
